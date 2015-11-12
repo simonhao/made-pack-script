4750d7d3db4d4357d8ba94927c999c03c6e099b9
IetnHao <dreamschool@163.com>
2015-11-12 17:48:38 +0800

    add pack

--- index.js
+++ index.js
@@ -7,3 +7,26 @@
 'use strict';
 
 var browserify = require('browserify');
+var path       = require('path');
+
+module.exports = function(options, done){
+  var base_path = options.basedir || path.join(process.cwd(), 'src');
+
+  var bundle = browserify({
+      paths: [base_path]
+  });
+
+  bundle.add(options.entry);
+  bundle.require(options.require);
+  bundle.external(options.external);
+
+  bundle.bundle(function(err, script){
+    if(err){
+        console.error(err.toString());
+        done && done(err, '');
+    }else{
+        done && done(null, script);
+    }
+  });
+
+};
